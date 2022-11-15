e89de76af7f94c14ea488c7f07be56a6b053a25e
Alan-Stephen <stephenalan101@gmail.com>
2022-11-15 16:06:14 +0000

    Initial Commit Transferred project into a javafx maven project Moved asset files into the resouces directory to fix linking issues.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.snakeproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.desktop;
+    requires jlayer;
+
+
+    exports com.example.snakeproject;
+}
