72ed49409c9f5f1bc6f66b4e52347e392ad7efb0
Alan-Stephen <stephenalan101@gmail.com>
2022-12-02 15:55:46 +0000

    javafx: add deathScene and menuScene
    add deathScene and menuScene to stage with
    appropriate logic to switch between them.

--- src/main/java/com/example/snakeproject/App.java
+++ src/main/java/com/example/snakeproject/App.java
@@ -1,28 +1,76 @@
 package com.example.snakeproject;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.security.Key;
 
+// TODO : make use of boolean statements
 public class App extends Application {
+
+    private boolean setUpDeath(Stage stage,GameController gc) throws IOException {
+
+        FXMLLoader deathLoader = new FXMLLoader(getClass().getResource("/deathScene.fxml"));
+        Scene deathScene = new Scene(deathLoader.load());
+        gc.snakeAliveProperty().addListener( (observableValue, oldVal, newVal) -> {
+            if(newVal){}
+            else {
+                stage.setScene(deathScene);
+            }
+        } );
+        DeathController deathController = deathLoader.getController();
+        deathController.submitButton.setOnAction(actionEvent -> {
+            deathController.addToLeaderBoards(gc.getMySnake().score);
+        });
+
+
+        return true;
+    }
+
+    private boolean play(Stage stage, FXMLLoader loader) {
+        try {
+            Scene gameScene = new Scene(loader.load());
+            GameController gameController = loader.getController();
+            GameKeyListener gkl = new GameKeyListener(gameController.getMySnake());
+
+            setUpDeath(stage,gameController);
+
+            gameScene.addEventHandler(KeyEvent.KEY_PRESSED, gkl);
+
+            System.out.println("settning scene");
+            stage.setScene(gameScene);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader myLoader = new FXMLLoader(getClass().getResource("/gameScene.fxml"));
+        FXMLLoader gameLoader = new FXMLLoader(getClass().getResource("/gameScene.fxml"));
+        FXMLLoader menuLoader = new FXMLLoader(getClass().getResource("/mainMenu.fxml"));
 
-        Scene scene = new Scene(myLoader.load());
-        GameController c = myLoader.getController();
+        GameController gameController = gameLoader.getController();
+
+        Scene menuScene = new Scene(menuLoader.load());
+        MenuController menuController = menuLoader.getController();
+        menuController.playButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println(play(stage,gameLoader));
+            }
+        });
 
 
-        GameKeyListener gkl = new GameKeyListener(c.mySnake);
-        scene.addEventHandler(KeyEvent.KEY_PRESSED,gkl);
+        stage.setScene(menuScene);
 
-        stage.setScene(scene);
         stage.show();
     }
 }
