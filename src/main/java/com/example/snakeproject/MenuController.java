c5693ac7ae5439d259899e5e51b7fd507538ce64
Alan-Stephen <stephenalan101@gmail.com>
2022-12-02 15:47:46 +0000

    javafx: add menu controller
    add controller class for menu.

---
+++ src/main/java/com/example/snakeproject/MenuController.java
@@ -0,0 +1,30 @@
+package com.example.snakeproject;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MenuController implements Initializable {
+    @FXML
+    AnchorPane menu;
+
+    @FXML
+    Button playButton;
+
+    @FXML
+    Button leaderboardButton;
+
+    @FXML
+    Button settingsButton;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+    }
+
+
+}
+
