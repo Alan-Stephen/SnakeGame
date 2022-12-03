dde6f9a1f0b0982963ca106355e539adcb9e6b70
Alan-Stephen <stephenalan101@gmail.com>
2022-12-03 14:53:32 +0000

    javafx: Create controller for leaderBoards.fxml
    create LeaderBoardController and implement
    updateList and intialise.

---
+++ src/main/java/com/example/snakeproject/LeaderBoardController.java
@@ -0,0 +1,65 @@
+package com.example.snakeproject;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
+public class LeaderBoardController implements Initializable {
+
+    public String CSV_PATH = "src/main/resources/images/leaderBoards.csv";
+
+    @FXML
+    TableView<LeaderBoardEntry> leaderBoardsTable;
+
+    @FXML
+    TableColumn<LeaderBoardEntry,String> name;
+
+    @FXML
+    TableColumn<LeaderBoardEntry, Integer> score;
+
+    @FXML
+    Button backButton;
+
+    ObservableList<LeaderBoardEntry> list = FXCollections.observableArrayList();
+
+    public boolean updateList() {
+        File file = new File(CSV_PATH);
+        list.clear();
+        try {
+            Scanner in = new Scanner(file);
+            while(in.hasNext()){
+                String entry[] = in.next().split(",");
+                list.add(new LeaderBoardEntry(entry[0], Integer.parseInt(entry[1]),0));
+                FXCollections.sort(list, Comparator.comparingInt(LeaderBoardEntry::getScore));
+                FXCollections.reverse(list);
+            }
+            in.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return true;
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        name.setCellValueFactory(
+                new PropertyValueFactory<LeaderBoardEntry,String>("name"));
+        score.setCellValueFactory(
+                new PropertyValueFactory<LeaderBoardEntry,Integer>("score"));
+        leaderBoardsTable.setItems(list);
+        updateList();
+    }
+}
