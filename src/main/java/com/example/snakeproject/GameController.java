cf414bd55584439981b079665a7a1e565b24eddb
Alan-Stephen <stephenalan101@gmail.com>
2022-12-03 21:41:42 +0000

    mvc: alter GameController to work with new design
    update GameController to work with split bewteen
    View and Model.

--- src/main/java/com/example/snakeproject/GameController.java
+++ src/main/java/com/example/snakeproject/GameController.java
@@ -1,19 +1,14 @@
 package com.example.snakeproject;
 
-import javafx.animation.Animation;
 import javafx.animation.AnimationTimer;
 import javafx.beans.property.BooleanProperty;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 
-import java.awt.*;
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -25,55 +20,65 @@ public class GameController implements Initializable {
     @FXML
     Canvas canvas;
 
-    private Snake mySnake = new Snake(100,100);
+    private SnakeModel snakeModel = new SnakeModel(100,100);
+    private SnakeView snakeView = new SnakeView();
 
-    public boolean hasFail(){return !mySnake.isL();}
 
-    public Snake getMySnake() {return mySnake;}
+    public SnakeModel getSnakeModel() {return snakeModel;}
+    public SnakeView getSnakeView() {return snakeView;}
 
-    public BooleanProperty snakeAliveProperty(){return mySnake.lProperty();}
+    public BooleanProperty snakeAliveProperty(){return snakeModel.activeProperty();}
 
     public boolean resetGame(){
-        this.mySnake = new Snake(100,100);
+        this.snakeModel = new SnakeModel(100,100);
+        snakeView = new SnakeView();
         return true;
     }
 
     private void startGame() {
-        Image background = ImageUtil.images.get("UI-background");
-        Image fail = ImageUtil.images.get("game-scene-01");
+        ImageUtil util = ImageUtil.getInstance();
+        Image background = util.getImage("UI-background");
 
         AnimationTimer loop = new AnimationTimer() {
             int frame = 0;
-            Food food = new Food();
+            FoodModel foodModel = new FoodModel();
+            FoodView foodView = new FoodView();
 
             GraphicsContext gc = canvas.getGraphicsContext2D();
+
+            //rewrite to have less indentation
             @Override
             public void handle(long l) {
                 frame++;
-                if(frame % 2 == 0){
-                    gc.drawImage(background, 0, 0);
-                    if (mySnake.isL())
-                    {
-                        mySnake.draw(gc);
-                        if (food.isL())
-                        {
-                            food.draw(gc);
-                            food.eaten(mySnake);
-                        } else
-                        {
-                            food = new Food();
-                        }
-                    } else
-                    {
-                        mySnake.setL(false);
-                    }
-                    mySnake.drawScore(gc);
-                }
+                if(frame % 2 == 0){}
+                else{return;}
 
+                gc.drawImage(background, 0, 0);
+
+                if(snakeModel.getActive()){}
+                else{return;}
+
+                snakeView.draw(gc, snakeModel.getX(), snakeModel.getY());
+                snakeView.drawBody(gc, snakeModel.bodyPoints);
+
+                if(foodModel.getActive()){
+                    foodView.draw(gc,foodModel.getX(), foodModel.getY());
+                    foodModel.eaten(snakeModel);
+                } else{
+                    foodModel = new FoodModel();
+                    foodView = new FoodView();
+                }
+                //todo: move draw score to this class
+                snakeView.drawScore(gc, snakeModel.score);
+                snakeModel.move();
+                snakeModel.updateBodyPoints(snakeView.bodyPointSpacing);
+                snakeModel.checkIfAlive();
             }
         };
         loop.start();
     }
+
+    // BUNCH OF FUCKING DRAW FUNCTIONS DOWN HERE
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
