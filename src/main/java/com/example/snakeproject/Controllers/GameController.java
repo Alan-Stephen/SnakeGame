f640fdb9a5353e635b7cffcb3d432afc397b86a5
Alan-Stephen <stephenalan101@gmail.com>
2022-12-06 15:53:32 +0000

    refactor: create DefaultGamemode to handle game
    create DefaultGamemode to handle gameloop instead
    of having logic directly in GameController.handle

--- src/main/java/com/example/snakeproject/Controllers/GameController.java
+++ src/main/java/com/example/snakeproject/Controllers/GameController.java
@@ -3,13 +3,11 @@ package com.example.snakeproject.Controllers;
 
 import com.example.snakeproject.*;
 import javafx.animation.AnimationTimer;
-import javafx.beans.property.BooleanProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
@@ -30,14 +28,12 @@ public class GameController implements Initializable {
     @FXML
     ImageView background;
 
-    GameLoop gameLoop;
 
-    AnimationTimer loop;
+    AnimationTimer timer;
 
     private SnakeModel snakeModel = new SnakeModel(100,100);
     private Theme theme = new
             Theme("gameBackground0","snake1");
-
 
 
     public SnakeModel getSnakeModel() {return snakeModel;}
@@ -55,54 +51,23 @@ public class GameController implements Initializable {
     }
 
     public void stopGame(){
-        loop.stop();
+        timer.stop();
     }
 
     public void startGame() {
-         loop = new AnimationTimer() {
 
+         timer = new AnimationTimer() {
             GraphicsContext gc = canvas.getGraphicsContext2D();
-            GameLoop gameLoop = new ImmortalGameLoop(theme,gc,snakeModel);
-
-            int frame = 0;
+            Gamemode gameLoop = new DefaultGamemode(theme,gc,snakeModel);
+             int frame = 0;
             FoodModel foodModel = new FoodModel();
 
             @Override
             public void handle(long l) {
-                System.out.println("hello");
-                //gameLoop.handle(l);
-                frame++;
-                if(frame % 2 == 0){}
-                else{return;}
-
-                if(snakeModel.getActive()){}
-                else{return;}
-
-                gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-
-                theme.drawSnake(gc, snakeModel.getX(),
-                        snakeModel.getY(),snakeModel.bodyPoints);
-
-                if(foodModel.getActive()){
-                    theme.drawFood(gc,foodModel.getX(), foodModel.getY());
-                    foodModel.eaten(snakeModel);
-                } else{
-                    foodModel = new FoodModel();
-                    theme.resetFood();
-                }
-                drawScore(gc, snakeModel.score);
-                snakeModel.move();
-                snakeModel.updateBodyPoints(getSnakeView().bodyPointSpacing);
-                snakeModel.checkIfAlive();
+                gameLoop.handle(l);
             }
         };
-        loop.start();
-    }
-
-    public void drawScore(GraphicsContext g,int score){
-        g.setFont(new Font("Comic Sans", 30));
-        g.setFill(Color.MAGENTA);
-        g.strokeText("SCORE : " + score, 20, 40);
+        timer.start();
     }
 
     @Override
---
+++ src/main/java/com/example/snakeproject/DefaultGamemode.java
@@ -0,0 +1,58 @@
+package com.example.snakeproject;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+public class DefaultGamemode implements Gamemode{
+
+    final int CANVAS_WIDTH = 870;
+
+    final int CANVAS_HEIGHT = 560;
+    GraphicsContext gc;
+    Theme theme;
+
+    int frame = 0;
+
+    SnakeModel snakeModel;
+
+    FoodModel foodModel = new FoodModel();
+    public DefaultGamemode(Theme theme, GraphicsContext gc, SnakeModel snakeModel) {
+        this.theme = theme;
+        this.gc = gc;
+        this.snakeModel = snakeModel;
+    }
+    @Override
+    public void handle(long l) {
+        frame++;
+        if(frame % 2 == 0){}
+        else{return;}
+
+        if(snakeModel.getActive()){}
+        else{return;}
+
+        gc.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+        theme.drawSnake(gc, snakeModel.getX(),
+                snakeModel.getY(),snakeModel.bodyPoints);
+
+        if(foodModel.getActive()){
+            theme.drawFood(gc,foodModel.getX(), foodModel.getY());
+            foodModel.eaten(snakeModel);
+        } else{
+            foodModel = new FoodModel();
+            theme.resetFood();
+        }
+        drawScore(gc, snakeModel.getScore());
+        snakeModel.move();
+        snakeModel.updateBodyPoints(theme.getSnakeView().bodyPointSpacing);
+        snakeModel.checkIfAlive();
+    }
+
+    public void drawScore(GraphicsContext g,int score){
+        g.setFont(new Font("Comic Sans", 30));
+        g.setFill(Color.MAGENTA);
+        g.strokeText("SCORE : " + score, 20, 40);
+    }
+
+}
