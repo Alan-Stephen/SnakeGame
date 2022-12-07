1dd2d5d18462fc29bb0a5b2485ca0e46786a2e41
Alan-Stephen <stephenalan101@gmail.com>
2022-12-07 20:17:29 +0000

    bugfix: solve unaccessible classes error
    solve error with classes being unaccessible from
    other packages

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,4 +9,8 @@ module com.example.snakeproject {
     exports com.example.snakeproject;
     exports com.example.snakeproject.Controllers;
     opens com.example.snakeproject.Controllers to javafx.fxml;
+    exports com.example.snakeproject.Views;
+    opens com.example.snakeproject.Views to javafx.fxml;
+    exports com.example.snakeproject.Model;
+    opens com.example.snakeproject.Model to javafx.fxml;
 }
