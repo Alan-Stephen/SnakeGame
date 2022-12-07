b582ef16f74226db4269dd5f8fa5b805b49ee418
Alan-Stephen <stephenalan101@gmail.com>
2022-12-07 20:06:43 +0000

    addition: add brick classes
    add BrickModel and BrickView to handle bricks
    which are an obstacle spawned depending on the
    difficulty selected.

---
+++ src/main/java/com/example/snakeproject/BrickModel.java
@@ -0,0 +1,11 @@
+package com.example.snakeproject;
+
+import com.example.snakeproject.Model.BombModel;
+import com.example.snakeproject.Model.ModelEntity;
+
+public class BrickModel extends BombModel {
+    public BrickModel(){
+        super();
+    }
+
+}
